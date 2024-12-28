971bef38449b1bad055ac3c31303ad6195e05611
asonkes <audrey.sonkes@gmail.com>
2024-12-28 14:58:49 +0100

    dernière modification js

--- assets/JS/includes/burger.js
+++ assets/JS/includes/burger.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const burgerUl = document.getElementById('burgerUl');
     const burgerLine = document.getElementById('burgerLine');
     const burgerNav = document.getElementById('burgerNav'); 
+    const burgerLi = document.querySelectorAll('.burgerLi');
 
     burgerIcon.addEventListener('click', () => {
         burgerUl.classList.toggle('active');
@@ -16,5 +17,11 @@ document.addEventListener('DOMContentLoaded', () => {
             burgerUl.classList.remove('active');
             burgerLine.classList.remove('active');
         }
+    })
+
+    burgerLi.forEach((e) => {
+        e.addEventListener('click', () => {
+            burgerUl.classList.remove('active');
+        })
     })
 })
