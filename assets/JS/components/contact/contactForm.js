aa8f610d50f60adf70a014e3d1707e637c81bd75
asonkes <audrey.sonkes@gmail.com>
2025-01-30 16:02:35 +0100

    modifications formulaire de contact en ordre

--- assets/JS/components/contact/contactForm.js
+++ assets/JS/components/contact/contactForm.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    if(document.location.hash === '#contact') {
-        history.replaceState({}, document.title, window.location.pathname);
-    }   
+    setTimeout(() => {
+        const formErrors = document.querySelectorAll('.formError');
+        // Ici on met false, car on considère par défault qu'il n'y a pas d'erreur
+        let hasErrors = false;
 
-    if (document.querySelector('.form-error-message')) {
-        window.location.hash = "contact"; // Ajoute #contact à l’URL pour scroller automatiquement
-    }
+        formErrors.forEach(error => {
+            // Si on enlève les espaces du contenu et que le texte n'est tout de même pas vide
+            // hasErrors = false (car cela veut dire qu'il y a du contenu, et donc des erreurs)
+            if (error.textContent.trim() !== '') {
+                hasErrors = true;
+            }
+        });
+
+        // Equivaut à hasErrors === true
+        if (hasErrors) {
+            document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+        } else if (document.location.hash === '#contact') {
+            history.replaceState({}, document.title, window.location.pathname);
+        }
+    }, 100);
 });
