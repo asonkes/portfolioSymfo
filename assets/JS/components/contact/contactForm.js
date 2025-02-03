6eb79fe1b72746b9eaa7cacb9b353543aafd20a8
asonkes <audrey.sonkes@gmail.com>
2025-02-03 23:39:46 +0100

    update

--- assets/JS/components/contact/contactForm.js
+++ assets/JS/components/contact/contactForm.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         const formErrors = document.querySelectorAll('.formError');
+        // Ici on met false, car on considère par défault qu'il n'y a pas d'erreur
+        let hasErrors = false;
 
-        if (formErrors) {
+        formErrors.forEach(error => {
+            // Si on enlève les espaces du contenu et que le texte n'est tout de même pas vide
+            // hasErrors = false (car cela veut dire qu'il y a du contenu, et donc des erreurs)
+            if (error.textContent.trim() !== '') {
+                hasErrors = true;
+            }
+        });
+        // Equivaut à hasErrors === true
+        if (hasErrors) {
             document.getElementById('contact').scrollIntoView();
+        } else if (document.location.hash === '#contact') {
+            history.replaceState({}, document.title, window.location.pathname);
         }
     }, 100);
 });
